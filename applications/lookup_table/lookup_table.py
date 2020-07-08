cce90715fd63496adb596a8d6b11f9c58c57c1c7
Victoria  Topham <topham.victoria@gmail.com>
2020-07-08 16:04:53 -0700

    lookup table runs in 10 seconds

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,8 @@
+# import math
+import random
+import math
 # Your code here
-
+lookup = {"hi": "hello"}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -14,7 +17,15 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    # make a key
+    key = f"{x}, {y}"
+
+    #if the key isn't stored, compute the value
+    print(lookup.get("ho"))
+    if lookup.get(key) == None:
+        lookup[key] = slowfun_too_slow(x, y)
+    return lookup.get(key)
+    #return the value of the key
 
 
 
