a57dfde9c0d233272cb27fefabf822afda271674
Victoria  Topham <topham.victoria@gmail.com>
2020-07-08 16:21:24 -0700

    expensive_seq is complete

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,10 +1,20 @@
 # Your code here
-
+lookup = {}
 
 def expensive_seq(x, y, z):
     # Your code here
+    if x <= 0: 
+        return y + z
+    
+    key = (x, y, z)
 
+    if lookup.get(key) != None:
+        return lookup.get(key)
 
+    elif x > 0: 
+        val = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+        lookup[key] = val
+        return val
 
 if __name__ == "__main__":
     for i in range(10):
