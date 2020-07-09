bc6e653038e094f5cc15d583101570c9dbca1cb2
Victoria  Topham <topham.victoria@gmail.com>
2020-07-08 17:05:44 -0700

    no dupes test

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,20 @@
 def no_dups(s):
     # Your code here
-
-
+    lookup = {}
+    output = []
+    #separate into an array of words
+    s = s.split(" ")
+    #check to see if the word is in the lookup. if it isn't store it.
+    #if it is, remove it from the array
+    for word in s:
+        if lookup.get(word) == None:
+            lookup[word] = 1
+            output.append(word)
+    #return the array as a string
+    output_string = ""
+    for word in output:
+        output_string += f" {word}"
+    return output_string[1:]
 
 if __name__ == "__main__":
     print(no_dups(""))
