64e5e794afb6756098e65d5306eab83e602d4b99
Victoria  Topham <topham.victoria@gmail.com>
2020-07-09 15:01:45 -0700

    fixed some indentation

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -4,20 +4,20 @@ import re
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
-    newWords = []
-    
-    words = re.split(' |\n|\r|\t', words)
-    to_remove = [ "\"", ':', ';', "?", ",", ".", "!", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&"]
-    
-    for word in words:
-        #clean the words
-        for char in to_remove:
-            word = word.replace(char, "")
-        word = word.lower()
-        if word != "":
-            newWords.append(word)
-    
-    # print(newWords)
+newWords = []
+
+words = re.split(' |\n|\r|\t', words)
+to_remove = [ "\"", ':', ';', "?", ",", ".", "!", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&"]
+
+for word in words:
+    #clean the words
+    for char in to_remove:
+        word = word.replace(char, "")
+    word = word.lower()
+    if word != "":
+        newWords.append(word)
+
+# print(newWords)
 
    
 
@@ -26,27 +26,27 @@ with open("input.txt") as f:
 # TODO: analyze which words can follow other words
 # Your code here
  #create hash table
-    lookup = {}
-    for i in range(len(newWords) - 1):
-        cur = newWords[i]
-        if lookup.get(cur) == None:
-            lookup[cur] = []
-        # print(f"Adding {newWords[i + 1]} as following {cur}")
-        lookup[cur].append(newWords[i + 1]) 
+lookup = {}
+for i in range(len(newWords) - 1):
+    cur = newWords[i]
+    if lookup.get(cur) == None:
+        lookup[cur] = []
+    # print(f"Adding {newWords[i + 1]} as following {cur}")
+    lookup[cur].append(newWords[i + 1]) 
 
 
 # TODO: construct 5 random sentences
 # Your code here
-    for x in range(5):
-        print(f"Sentence {x}:")
-        sentence = ""
-        curWord = random.choice(list(lookup))
-        for w in range(random.randint(5,20)):
-            sentence += curWord
-            sentence += " "
-            options = lookup[curWord]
-            curWord = random.choice(options)
-        print(sentence[:-1] + ".")
+for x in range(5):
+    print(f"Sentence {x}:")
+    sentence = ""
+    curWord = random.choice(list(lookup))
+    for w in range(random.randint(5,20)):
+        sentence += curWord
+        sentence += " "
+        options = lookup[curWord]
+        curWord = random.choice(options)
+    print(sentence[:-1] + ".")
             
 
 
